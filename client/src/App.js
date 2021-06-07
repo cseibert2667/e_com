ed279a0c430b3a4b8bdf3737134a4214af15c82a
cseibert2667 <cseibert2667@gmail.com>
2021-06-06 21:31:03 -0700

    installed react-router and used this to create switches for each page placeholder in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/profile" component={Profile} />
+      </Switch>
+    </Router>
   );
 }
-
 
 export default App;
