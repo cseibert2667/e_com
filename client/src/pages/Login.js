d7b5401f4b46af2f3e2fa3dc4d37f1a17fcafd3d
cseibert2667 <cseibert2667@gmail.com>
2021-06-08 22:26:16 -0700

    added some notation on pages for functionality ideas

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function Login() {
+    // will be looking into google authentication for login/signup feature
     return (
         <div>
             <h1>Login</h1>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export default function Profile() {
+    //show user posts, info, contact preferences etc. with options to edit, delete
+    //messaging feature? notifications? "follow" other users?
     return (
         <div>
             <h1>Profile</h1>
