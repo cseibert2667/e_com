dd846bfb49bf6c1eea01ddf83f38acdad5b40e00
cseibert2667 <cseibert2667@gmail.com>
2021-06-08 22:20:24 -0700

    added search bar to nav, other minor text changes

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,27 +2,45 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl"; 
 import Container from "react-bootstrap/Container";
 
 function Header() {
+  //write code for if/else statement on line 35 here
   return (
+    
     <>
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="/">CoopsList</Navbar.Brand>
+        <Navbar.Brand href="/">Coop'sList</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/">Search</Nav.Link>
-            <Nav.Link href="/saved">Saved</Nav.Link>
+            <Nav.Link href="/">Browse</Nav.Link>
+            <Nav.Link href="/saved">Saved</Nav.Link> 
           </Nav>
         </Navbar.Collapse>
+        <Container>
+        <InputGroup className="mb-3">
+        <InputGroup.Prepend>
+          <InputGroup.Text id="inputGroup-sizing-default">
+            Search
+          </InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl
+          aria-label="Default"
+          aria-describedby="inputGroup-sizing-default"
+        />
+      </InputGroup>
+      {/*if not logged in show "Login/Signup"; else, show "My Profile" */}
+      </Container>
       </Navbar>
 
       <Jumbotron fluid>
         <Container className="text-center">
-          <h1>(React) Google Books Search</h1>
+          <h1>Coop'sList - Buy, Sell, Trade</h1>
           <p>
-            Search for and Save Books of Interest
+            Browse local listings, search, or filter
           </p>
         </Container>
       </Jumbotron>
