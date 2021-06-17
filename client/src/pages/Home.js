9ffe9e8a3f5c7fa97fb1014118ff20a3a5856e52
cseibert2667 <cseibert2667@gmail.com>
2021-06-17 07:29:52 -0700

    posts rendered to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,13 @@
 // Possibly call this "Browse" where categories would be listed
 
 import React from 'react'
+import Post from "../components/Post/Post"
 
 export default function Home() {
     return (
         <div>
             <h1>Home page</h1>
+            <Post/>
         </div>
     )
 }
