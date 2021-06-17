16d3b233f618b58fc0b1d9bb6cccc9ac7779eda1
cseibert2667 <cseibert2667@gmail.com>
2021-06-17 07:27:44 -0700

    card layout for posts

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Card from "react-bootstrap/Card";
 
 export default function Post() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+          <small className="test-muted">Posted 69 minutes ago</small>
+      </Card.Footer>
+    </Card>
+  );
 }
